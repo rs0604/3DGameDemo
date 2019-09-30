f6c0153a75ec589a160c2db686285a501d92770a
shima@luxor-system.jp <shima@luxor-system.jp>
2019-09-30 23:53:23 +0900

    first commit

---
+++ Assets/PlayerFollowCamera.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFollowCamera : MonoBehaviour {
+    // 注視対象プレイヤー
+    [SerializeField] private Transform player;
+    // 注視対象プレイヤーとの距離
+    [SerializeField] private float distance = 4.0f;
+    // カメラの垂直回転
+    [SerializeField] private Quaternion vRotation;
+    // カメラの水平回転
+    [SerializeField] public Quaternion hRotation;
+    // 回転速度
+    [SerializeField] private float turnSpeed = 10.0f;
+    
+    // Start is called before the first frame update
+    void Start() {
+        // 回転の初期化
+        vRotation = Quaternion.Euler(30, 0, 0);
+        hRotation = Quaternion.identity;
+        
+        // 位置の初期化
+        // player位置から距離distanceだけ手前に引いた位置を設定
+        transform.position = player.position - transform.rotation
+                             * Vector3.forward * distance;
+    }
+
+    // Update is called once per frame
+    void Update() { }
+
+    void LateUpdate() {
+        // 水平回転の更新
+        
+        // カメラの位置(transform.position)の更新
+        // player位置から距離distanceだけ手前に引いた位置を設定
+        transform.position = player.position - transform.rotation 
+                             * Vector3.forward * distance;
+    }
+}
---
+++ Assets/Robot Kyle/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField] private Vector3 velocity;
+
+    [SerializeField] private float moveSpeed = 5.0f;
+    
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        velocity = Vector3.zero;
+        if (Input.GetKey(KeyCode.E)) {
+            velocity.z += 1;
+        }
+        if (Input.GetKey(KeyCode.S)) {
+            velocity.x -= 1;
+        }
+        if (Input.GetKey(KeyCode.D)) {
+            velocity.z -= 1;
+        }
+        if (Input.GetKey(KeyCode.F)) {
+            velocity.x += 1;
+        }
+
+        // 速度ベクトルの長さを1秒でmoveSpeedだけ進むように調整する
+        velocity = velocity.normalized * moveSpeed * Time.deltaTime;
+        
+        // いずれかの方向に移動している場合
+        if (velocity.magnitude > 0) {
+            // プレイヤーの位置(transform.position)の更新
+            // 移動方向ベクトル(velocity)を足す
+            transform.position += velocity;
+        }
+
+    }
+}
